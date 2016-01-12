252008a3d0bb3d02484aefb4c72abe76032fa409
Adrian Cooney <cooney.adrian@gmail.com>
2016-01-12 01:27:26 +0000

    SASS with Webpack compilation.
    Added in some pretty sweet CSS loading via webpack. Components now
    have their own CSS file and all.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,13 +2,16 @@ var path = require("path");
 
 module.exports = {
     context: __dirname,
-    entry: "./src/index.js",
+    entry: "./src/index.jsx",
 
     module: {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loaders: ["react-hot", "babel?presets[]=react&presets[]=es2015"]
+        }, {
+            test: /\.scss$/,
+            loaders: ["style", "css", "sass"]
         }]
     },
 
