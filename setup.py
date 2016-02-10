bb10fcf41be759230828054912b0872064ee37c7
Adrian Cooney <cooney.adrian@gmail.com>
2016-02-10 13:29:24 +0000

    Python package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'Examist',
+    version = '0.0.3',
+    description = 'Examist Server',
+    author = 'Adrian Cooney',
+    author_email = 'cooney.adrian@gmail.com',
+    url = 'https://github.com/examist',
+    packages = ['server']
+)
