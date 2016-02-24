e69e6fa2c27b64759dfb28a63b0dacc2a0c5fb12
Adrian Cooney <cooney.adrian@gmail.com>
2016-02-24 20:43:11 +0000

    server/test/api refactored.

--- server/test/api/module_blueprint_test.py
+++ server/test/api/module_blueprint_test.py
@@ -1,31 +1,31 @@
 from server.test import assert_api_error
 from json import dumps, loads
 
-def test_module_search_no_query(auth_client):
-    assert_api_error(auth_client.get("/module/search"), 422, meta={"field": "q"})
+def test_course_search_no_query(auth_client):
+    assert_api_error(auth_client.get("/course/search"), 422, meta={"field": "q"})
 
-def test_module_search_no_login(client):
-    assert_api_error(client.get("/module/search?q=foo"), 401)
+def test_course_search_no_login(client):
+    assert_api_error(client.get("/course/search?q=foo"), 401)
 
-def test_module_search_empty(auth_client, modules):
-    resp = auth_client.get("/module/search?q=Ridiculous+query")
+def test_course_search_empty(auth_client, courses):
+    resp = auth_client.get("/course/search?q=Ridiculous+query")
     data = loads(resp.get_data())
-    assert len(data["modules"]) == 0
+    assert len(data["courses"]) == 0
 
-def test_module_search(auth_client, modules):
-    resp = auth_client.get("/module/search?q=computer+security")
+def test_course_search(auth_client, courses):
+    resp = auth_client.get("/course/search?q=computer+security")
     data = loads(resp.get_data())
-    assert len(data["modules"]) > 0
+    assert len(data["courses"]) > 0
 
-def test_module_get(auth_client, module_with_papers):
-    module = module_with_papers
-    resp = auth_client.get("/module/" + module.code.lower())
+def test_course_get(auth_client, course_with_papers):
+    course = course_with_papers
+    resp = auth_client.get("/course/" + course.code.lower())
     assert resp.status_code == 200
 
-    data = loads(resp.get_data())["module"]
-    assert data["id"] == module.id
+    data = loads(resp.get_data())["course"]
+    assert data["id"] == course.id
     assert len(data["papers"]) == 5
 
-def test_module_get_not_found(auth_client):
-    resp = auth_client.get("/module/foobar")
+def test_course_get_not_found(auth_client):
+    resp = auth_client.get("/course/foobar")
     assert_api_error(resp, 404)
--- server/test/api/profile_blueprint_test.py
+++ server/test/api/profile_blueprint_test.py
@@ -2,43 +2,43 @@ from server.test import assert_api_error
 from server.library.util import find
 from json import loads
 
-def test_profile_modules_empty(auth_client):
-    resp = auth_client.get("/profile/modules")
+def test_profile_courses_empty(auth_client):
+    resp = auth_client.get("/profile/courses")
 
     assert resp.status_code == 200
 
     data = loads(resp.get_data())
-    assert isinstance(data["modules"], list)
-    assert len(data["modules"]) == 0
+    assert isinstance(data["courses"], list)
+    assert len(data["courses"]) == 0
 
-def test_profile_modules(auth_client, user_with_modules):
-    resp = auth_client.get("/profile/modules")
+def test_profile_courses(auth_client, user_with_courses):
+    resp = auth_client.get("/profile/courses")
 
     assert resp.status_code == 200
 
     data = loads(resp.get_data())
-    assert isinstance(data["modules"], list)
-    assert len(data["modules"]) == 5
+    assert isinstance(data["courses"], list)
+    assert len(data["courses"]) == 5
 
-def test_profile_add_module(auth_client, user, modules, session):
-    newModule = modules[0]
-    resp = auth_client.patch("/profile/modules", data={ "module": newModule.id })
+def test_profile_add_course(auth_client, user, courses, session):
+    newModule = courses[0]
+    resp = auth_client.patch("/profile/courses", data={ "course": newModule.id })
     assert resp.status_code == 200
 
     session.refresh(user)
-    assert len(user.modules) > 0
+    assert len(user.courses) > 0
 
-def test_profile_add_module_existing(auth_client, user_with_modules, session):
-    existingModule = user_with_modules.modules[0]
-    resp = auth_client.patch("/profile/modules", data={ "module": existingModule.id })
+def test_profile_add_course_existing(auth_client, user_with_courses, session):
+    existingModule = user_with_courses.courses[0]
+    resp = auth_client.patch("/profile/courses", data={ "course": existingModule.id })
     assert resp.status_code == 200
-    session.refresh(user_with_modules)
-    assert len(user_with_modules.modules) == 5
+    session.refresh(user_with_courses)
+    assert len(user_with_courses.courses) == 5
 
-def test_profile_add_module(auth_client, user_with_modules, session):
-    existingModule = user_with_modules.modules[0]
-    resp = auth_client.delete("/profile/modules", data={ "module": existingModule.id })
+def test_profile_add_course(auth_client, user_with_courses, session):
+    existingModule = user_with_courses.courses[0]
+    resp = auth_client.delete("/profile/courses", data={ "course": existingModule.id })
     assert resp.status_code == 200
-    session.refresh(user_with_modules)
-    assert len(user_with_modules.modules) == 4
-    assert not find(user_with_modules.modules, lambda mod: mod.id == existingModule.id)
+    session.refresh(user_with_courses)
+    assert len(user_with_courses.courses) == 4
+    assert not find(user_with_courses.courses, lambda mod: mod.id == existingModule.id)
