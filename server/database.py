4c24fda894f0e3e9c90c3d574746926350e81dfe
Adrian Cooney <cooney.adrian@gmail.com>
2016-03-10 18:25:22 +0000

    Installing latest version of Flask SQL Alchemy and adding in our JSON serializable model base aswell as flask-sqlalchemy's query base via composition.

--- server/database.py
+++ server/database.py
@@ -1,7 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy, Model
+from flask.ext.jsontools import JsonSerializableBase
 from server import config
 
-db = SQLAlchemy()
+class ModelBase(Model, JsonSerializableBase):
+    pass
+
+db = SQLAlchemy(model_class=ModelBase)
 
 # For nicer exports
 Model = db.Model
