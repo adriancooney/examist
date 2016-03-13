157ef3b265bb68ca18559298972468f97148e71f
Adrian Cooney <cooney.adrian@gmail.com>
2016-03-13 18:18:31 +0000

    Major update to test output.

--- server/test/conftest.py
+++ server/test/conftest.py
@@ -3,7 +3,10 @@ import traceback
 import random
 import os
 import json
+import logging
+import sys
 from sqlalchemy_utils import database_exists, create_database, drop_database
+from server.test.conflogging import setup_logging
 from server.test import APIClient
 from server.web import app as _app
 from server.middleware import AUTH_HEADER_NAME
@@ -12,6 +15,9 @@ from server.database import db as _db
 from server import model
 
 DB_NAME = config.DB_NAME + "_test"
+
+setup_logging()
+logger = logging.getLogger(__name__)
 
 # Data generation
 with open(os.path.join(os.path.dirname(__file__), "data/example.json")) as data_file:
@@ -30,7 +36,7 @@ def app(request):
 
     # Config the app
     _app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-    _app.config["SQLALCHEMY_ECHO"] = False # Toggle SQL Alchemy output
+    _app.config["SQLALCHEMY_ECHO"] = True # Toggle SQL Alchemy output
     _app.config["TESTING"] = True
 
     # Establish an application context before running the tests.
@@ -62,7 +68,6 @@ def client(db, app):
 @pytest.fixture
 def session(db, monkeypatch, request):
     """Creates a new database session for a test."""
-    print marker("SESSION START (%s/%s)" % (request.module.__name__, request.function.__name__))
     connection = db.engine.connect()
     transaction = connection.begin()
 
@@ -73,7 +78,6 @@ def session(db, monkeypatch, request):
         transaction.rollback()
         connection.close()
         db.session.remove()
-        print marker("SESSION ROLLBACK (%s/%s)" % (request.module.__name__, request.function.__name__))
 
     request.addfinalizer(teardown)
     return db.session
@@ -138,12 +142,58 @@ def papers(session):
     return papers
 
 @pytest.fixture
+def questions(session):
+    questions = []
+
+    def push_question(question_data, index, parent=None):
+        content = question_data.get("content", None)
+        marks = question_data.get("marks", None)
+
+        index_type = question_data.get("index_type")
+        formatted_index = model.Question.format_index(index_type, index)
+
+        path = (index,) if not parent else parent.path + (index,)
+        formatted_path = (formatted_index,) if not parent else parent.formatted_path + (formatted_index,)
+
+        question = model.Question(None, index, index_type, path, formatted_path, parent=parent)
+
+        if content:
+            question.set_content(None, content)
+
+        questions.append(question)
+
+        if "questions" in question_data:
+            for i, child_question_data in enumerate(question_data["questions"]):
+                push_question(child_question_data, i + 1, parent=question)
+
+    session.add_all(questions)
+    session.flush()
+
+    for i, question in enumerate(data["questions"]):
+        push_question(question, i + 1)
+
+    return questions
+
+@pytest.fixture
 def course_with_papers(session, papers, courses):
     course = courses[0]
     course.papers = papers
     session.add(course)
     session.flush()
     return course
+
+@pytest.fixture
+def paper_with_course_and_questions(session, papers, courses, questions):
+    course = courses[0]
+    paper = papers[0]
+
+    # Assign the data
+    paper.course = course
+    paper.questions = questions
+
+    session.add(paper)
+    session.flush()
+    return paper
 
 @pytest.fixture
 def user_with_courses(user, courses, session):
@@ -163,5 +213,21 @@ def auth_client(user, session, client):
     
     return client
 
+@pytest.yield_fixture(autouse=True)
+def output_logger(request):
+    logger.info(marker("TEST START (%s/%s)" % (request.module.__name__, request.function.__name__)))
+    yield
+    logger.info(marker("TEST END (%s/%s)" % (request.module.__name__, request.function.__name__)))
+
 def marker(text, spacer="-", size=40):
     return "\n\n{} {} {}\n".format(spacer*size, text, spacer*size)
+
+# Ignore SQL Alchemy output during test setup
+sqla_logger = logging.getLogger("sqlalchemy.engine.base.Engine")
+sqla_logger.setLevel(logging.CRITICAL)
+
+def pytest_runtest_call():
+    logging.getLogger("sqlalchemy.engine.base.Engine").handlers[0].setLevel(logging.INFO)
+
+def pytest_runtest_teardown():
+    logging.getLogger("sqlalchemy.engine.base.Engine").handlers[0].setLevel(logging.CRITICAL)
