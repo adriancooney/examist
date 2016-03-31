f87ee6d7cf3375589a9ecb64723affab1d0dd446
Adrian Cooney <cooney.adrian@gmail.com>
2016-03-31 20:26:08 +0100

    Style fixes and error 404 pages uses updated ErrorPage component.

--- client/src/views/pages/Error404.jsx
+++ client/src/views/pages/Error404.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Link } from "react-router";
 import ErrorPage from "../ui/error/ErrorPage";
 
 export default function Error404() {
-    return <ErrorPage code="404" message="Page Not Found." />;
+    return (
+        <ErrorPage code="404">
+            <p>Page not found. <Link to="/">Go home.</Link></p>
+        </ErrorPage>
+    );
 }
