c6a1fc4ed786bcbf2a92130ebcff6eb0171ba1ee
Adrian Cooney <cooney.adrian@gmail.com>
2016-04-03 20:39:52 +0100

    Updating logging.

--- server/cache.py
+++ server/cache.py
@@ -1,6 +1,9 @@
+import logging
 from functools import wraps
 from flask import current_app
 from flask.ext.cache import Cache 
+
+logger = logging.getLogger(__name__)
 
 # Setup the cache. See init.py for attaching to app.
 cache = Cache(config=dict(
@@ -15,7 +18,7 @@ def invalidate_view(name, *args, **kwargs):
 
 def cache_view(f=None, timeout=None, make_name=None, unless=None):
     def decorator(f):
-        print "Caching view function: %s" % f.__name__
+        logger.debug("Caching view function: %s" % f.__name__)
         return cache.memoize(timeout, make_name, unless)(f)
 
     if f:
--- server/init.py
+++ server/init.py
@@ -1,19 +1,26 @@
 import logging
 import sys
 
-# Setup the logging
-root = logging.getLogger()
-stdout = logging.StreamHandler(stream=sys.stdout)
-root.addHandler(stdout)
-
 from server import config
 from server.database import db
 from server.cache import cache, get_view_func
 from server.web import app
 
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
     db.init_app(app)
     cache.init_app(app)
+
+    logger = logging.getLogger("server")
+    handler = logging.StreamHandler(stream=sys.stdout)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    items = [(key, value) for key, value in config.__dict__.iteritems() if key.startswith("APP_") or key.startswith("DB_")]
+
+    for key, value in sorted(items, key=lambda t: t[0]):
+        logger.info("{:>20}: {}".format(key, str(value)))
 
     # And off we go!
     app.run(port=config.APP_PORT, host=config.APP_HOST, debug=config.APP_DEBUG)
